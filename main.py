9cbc59ab5654c2942915c27d669474b5e0b74c5e
ROHITMEENA64 <157285469+ROHITMEENA64@users.noreply.github.com>
2025-04-03 11:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ async def remove_user(client, message: Message):
     except ValueError:
         await message.reply_text("Invalid command format. Use: /removeuser user_id")
 
-YOUR_ADMIN_ID = 6486192717
+YOUR_ADMIN_ID = 6414266397
 
 # Helper function to check admin privilege
 def is_admin(user_id):
